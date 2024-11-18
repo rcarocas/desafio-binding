444ce88816895b6a79607594d4f8067582019e15
Rocio Caro Casanueva <rcarocasanueva@gmail>
2024-11-18 18:11:49 -0300

    change url

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Desaf-o-tarjeta" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/desafio-binding/" : "/",
 };
