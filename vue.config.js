96583622a3e67c15d70c20749c00ad9996ce52c1
Rocio Caro Casanueva <rcarocasanueva@gmail>
2024-11-18 17:56:38 -0300

    name correction

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/desafio-binding" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Desaf-o-tarjeta" : "/",
 };
